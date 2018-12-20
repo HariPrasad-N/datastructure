8e1314b46c7977524af688633693520a913d1f09
HariPrasad-N <narasimhaiah.hariprasad@gmail.com>
2018-12-20 20:32:18 +0530

    Started linked_list

---
+++ linked_list/linked_list.py
@@ -0,0 +1,38 @@
+class Node:
+	def __init__(self,data=None,next=None):
+		"""
+		attributes:
+			-> data = data in the node
+			-> next = next node it points to
+		"""
+		self.data=data
+		self.next=next
+
+	def __str__(self):
+		return str(self.data)
+
+	def __repr__(self):
+		return str(self.data)
+
+	def __add__(self,rnode):
+		"""
+		adds the data of two Node variables and returns data
+		Raises TypeError if type of data attribute does not support substraction
+		"""
+		return self.data+rnode.data
+
+	def __sub__(self,rnode):
+		"""
+		substracts the data of two Node variables and returns data
+		Raises TypeError if type of data attribute does not support substraction
+		"""
+		return self.data-rnode.data
+
+	
+
+if __name__ == '__main__':
+	node1 = Node(1)
+	node2 = Node(2)
+	print(node1,'+',node2,'=',node1+node2)
+	a=node1-node2
+	print(a)
