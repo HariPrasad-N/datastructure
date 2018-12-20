58e245823f3867d2400dd32a03f3b409c450b2cb
Hari Prasad N <narasimhaiah.hariprasad@gmail.com>
2018-12-20 22:49:36 +0530

    Node class update1
    Added all operations for node class

--- linked_list/Node.py
+++ linked_list/Node.py
@@ -14,25 +14,104 @@ class Node:
 	def __repr__(self):
 		return str(self.data)
 
+	def	__neg__(self):
+		"""
+		returns negation(a.data) for a Node
+		Raises TypeError if type of data attribute does not support negation
+		"""
+		return self.data*-1
+
 	def __add__(self,rnode):
 		"""
-		adds the data of two Node variables and returns data
-		Raises TypeError if type of data attribute does not support substraction
+		returns a.data+b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support addition
 		"""
 		return self.data+rnode.data
 
 	def __sub__(self,rnode):
 		"""
-		substracts the data of two Node variables and returns data
+		returns a.data-b.data for a and b Nodes
 		Raises TypeError if type of data attribute does not support substraction
 		"""
 		return self.data-rnode.data
 
-	
+	def __mul__(self,rnode):
+		"""
+		returns a.data*b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support multiplication
+		"""
+		return self.data*rnode.data
+
+
+	def	__floordiv__(self,rnode):
+		"""
+		returns a.data//b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support floor division
+		"""
+		return self.data//rnode.data
+
+	def	__truediv__(self,rnode):
+		"""
+		returns a.data/b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support division
+		"""
+		return self.data/rnode.data
+
+	def __lt__(self,rnode):
+		"""
+		returns a.data<b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support logical operations
+		"""
+		return self.data < rnode.data
+
+	def __le__(self,rnode):
+		"""
+		returns a.data<=b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support logical operations
+		"""
+		return self.data <= rnode.data 
+
+	def __eq__(self,rnode):
+		"""
+		returns a.data==b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support logical operations
+		"""
+		return self.data == rnode.data
+
+	def __ne__(self,rnode):
+		"""
+		returns a.data!=b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support logical operations
+		"""
+		return self.data != rnode.data
+
+	def __ge__(self,rnode):
+		"""
+		returns a.data>=b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support logical operations
+		"""
+		return self.data >= rnode.data
+
+	def __gt__(self,rnode):
+		"""
+		returns a.data>b.data for a and b Nodes
+		Raises TypeError if type of data attribute does not support logical operations
+		"""
+		return self.data > rnode.data
 
 if __name__ == '__main__':
 	node1 = Node(1)
 	node2 = Node(2)
-	print(node1,'+',node2,'=',node1+node2)
-	a=node1-node2
-	print(a)
+	print(-node1)
+	print(node1+node2)
+	print(node1-node2)
+	print(node1*node2)
+	print(node1//node2)
+	print(node1/node2)
+	print(node1<node2)
+	print(node1<=node2)
+	print(node1==node2)
+	print(node1!=node2)
+	print(node1>=node2)
+	print(node1>node2)
+	
